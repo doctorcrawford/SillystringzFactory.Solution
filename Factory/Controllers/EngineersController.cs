1bfcf7d50c975d76135a6eaedc4b6b08cf40b378
Kyle Crawford <kcraw217@gmail.com>
2023-05-26 10:56:33 -0700

    add create, details, edit and delete functionality

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -18,6 +18,64 @@ public class EngineersController : Controller
 
   public ActionResult Index()
   {
+    List<Engineer> model = _db.Engineers.ToList();
+    return View(model);
+  }
+
+  public ActionResult Create()
+  {
     return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Engineer engineer)
+  {
+    if (!ModelState.IsValid)
+    {
+      return View(engineer);
+    }
+    else
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+
+  public ActionResult Details(int id)
+  {
+    Engineer thisEngineer = _db.Engineers
+          .FirstOrDefault(engineer => engineer.EngineerId == id);
+    return View(thisEngineer);
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+    // ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View(thisEngineer);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Engineer engineer)
+  {
+    _db.Engineers.Update(engineer);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+    return View(thisEngineer);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+    _db.Engineers.Remove(thisEngineer);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
   }
 }
