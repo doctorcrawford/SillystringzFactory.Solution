3f9fbd2ff3e2e63e915532842f88c340a94c6cae
Kyle Crawford <kcraw217@gmail.com>
2023-05-26 10:40:48 -0700

    add delete and edit functionality for machines

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -46,6 +46,36 @@ public class MachinesController : Controller
   {
     Machine thisMachine = _db.Machines
           .FirstOrDefault(machine => machine.MachineId == id);
-          return View(thisMachine);
+    return View(thisMachine);
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    // ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View(thisMachine);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Machine machine)
+  {
+    _db.Machines.Update(machine);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    return View(thisMachine);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Machine thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+    _db.Machines.Remove(thisMachine);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
   }
 }
