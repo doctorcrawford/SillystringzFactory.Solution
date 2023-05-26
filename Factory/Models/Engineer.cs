3b47232f44ebf44c59a500ae0791a464574e999a
Kyle Crawford <kcraw217@gmail.com>
2023-05-26 10:00:31 -0700

    add input validation

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Engineer
 {
   public int EngineerId { get; set; }
+  [Required(ErrorMessage = "The engineer must have a name!")]
   public string Name { get; set; }
   public List<Machine> Machines { get; set; }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models;
 
 public class Machine
 {
   public int MachineId { get; set; }
+  [Required(ErrorMessage = "The machine must have a name!")]
   public string Name { get; set; }
   public List<Engineer> Engineers { get; set; }
 }
